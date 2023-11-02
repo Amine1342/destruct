f3c6d667d2c1b0089cb4d1e27158d5c6a2a17dda
Amine1342 <benabdelkader.amine1@gmail.com>
2023-11-02 23:04:16 +0100

    destruct 9:37

---
+++ src/Components/Display.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Users from "./Singers";
+
+class Display extends Component {
+  render() {
+    return (
+      <div>
+        <Users />
+      </div>
+    );
+  }
+}
+
+export default Display;
---
+++ src/Components/Singers.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Users = () => {
+  return (
+    <div>
+      <h2 className="text-primary mt-3">Chanteur :</h2>
+    </div>
+  );
+};
+
+export default Users;

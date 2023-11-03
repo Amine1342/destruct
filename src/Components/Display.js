b418b4d6f817245f4940b1808f01cf9f090afb40
Amine1342 <benabdelkader.amine1@gmail.com>
2023-11-03 22:31:32 +0100

    Fin

--- src/Components/Display.js
+++ src/Components/Display.js
@@ -5,7 +5,11 @@ class Display extends Component {
   render() {
     return (
       <div>
-        <Users />
+        <h1 className="text-danger mt-3 mb-3">SINGERS</h1>
+        <Users name="Kery James" age="45" />
+        <Users name="Medine" age="37" />
+        <Users name="Lotfi DK" age="40" />
+        <Users name="Bob Marley" age="67" />
       </div>
     );
   }
--- src/Components/Singers.js
+++ src/Components/Singers.js
@@ -1,11 +1,22 @@
 import React from "react";
 
-const Users = () => {
-  return (
-    <div>
-      <h2 className="text-primary mt-3">Chanteur :</h2>
-    </div>
-  );
-};
+class Users extends React.Component {
+  render() {
+    const { name, age } = this.props;
+
+    return (
+      <div>
+        <h4 className="text-primary mt-3">Chanteur :</h4>
+        <p className="text-success mt-3">
+          <strong>Nom : </strong>
+          {name}
+          <br />
+          <strong>Age : </strong>
+          {age}
+        </p>
+      </div>
+    );
+  }
+}
 
 export default Users;
